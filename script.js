1e416cf46c2b16f5bb59e029fced6d90c978a491
Hugo Brito <hugomdmb@gmail.com>
2022-09-09 16:56:17 -0300

    primeiro commit da entrega S7-17

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 
+let count = [0,0,0,0,0,0,0,0,0,0,0,0,0]
+
+let divResultados = document.querySelector('.resultados')
+for (let i = 0; i < 1000; i++) {
+    let dice1 = Math.floor(Math.random() * 6 + 1);
+    let dice2 = Math.floor(Math.random() * 6 + 1);
+
+    divResultados.insertAdjacentHTML('beforeend',`
+    <p>Lançamento: ${i}   /   Primeiro Dado: ${dice1}   /   Segundo Dado: ${dice2}   /   Soma: ${dice1 + dice2}</p>
+    `)
+
+    // console.log(dice1)
+    // console.log(dice2)
+    count[dice1 + dice2] += 1
+    
+}
+
+console.log(count)
+
+let divGrafico = document.querySelector('.grafico')
+divGrafico.insertAdjacentHTML('afterbegin', `<h1>Gráfico de Rolagens</h1>`)
+
+for(let i = 2 ; i < count.length; i++){
+    divGrafico.insertAdjacentHTML('beforeend',`
+    <p style="background-color:#000000; height: 15px; width: ${count[i]*3}px;color: #ffffff; font-size: 10px">Soma ${i} (${count[i]}x)</p>
+    `)
+    
+}
